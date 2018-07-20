ae1e42f20d4bbd2d63132bc044fc59dadca51bd9
Jeff Lewis <jeffylew77@gmail.com>
2018-07-20 00:56:37 -0700

    [Dev] Disabling Mongoose for the time being

--- server/server.js
+++ server/server.js
@@ -8,27 +8,27 @@ import SCHEMA from './graphql/schema.js';
 
 // Imports: Mongoose
 import mongoose from 'mongoose';
-import mongoURI from '../config/mongo-uri.js';
+// import database from '';
 
 // Imports: Middleware
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 
-// Database: Connection
-mongoose.connect(
-  mongoURI,
-  { useNewUrlParser: true },
-  // Error Handling
-  (err) => {
-    if(err) {
-      console.log(`Unable to connect to the Database. Error: ${err}`);
-    }
-    else {
-      console.log(`Connected to Database.`);
-    }
-  }
-);
+// // Database: Connection
+// mongoose.connect(
+//   mongoURI,
+//   { useNewUrlParser: true },
+//   // Error Handling
+//   (err) => {
+//     if(err) {
+//       console.log(`Unable to connect to the Database. Error: ${err}`);
+//     }
+//     else {
+//       console.log(`Connected to Database.`);
+//     }
+//   }
+// );
 
 
 // Middleware: CORS
