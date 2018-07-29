1fed6fd979b30517c0f60bc46df1c770703d5ca1
Jeff Lewis <jeffylew77@gmail.com>
2018-07-28 22:07:38 -0700

    [Fix] Fixing browser title to be Gov Hub

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -9,7 +9,7 @@ class NavagationBar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg bg-light">
           <a class="navbar-brand" href="#">GovHub</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
