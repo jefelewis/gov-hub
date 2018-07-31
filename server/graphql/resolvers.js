eeb1de3d0f92529ff4b512e4febef3ff77e5e436
Jeff Lewis <jeffylew77@gmail.com>
2018-07-31 14:21:20 -0700

    [Dev] Migrating from Apollo-Server-Express 1.4.0 to 2.0.0

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,16 +1,23 @@
 // Imports: Axios
 import axios from 'axios';
 
+// Imports: Node.js File System
+import fs from 'fs';
+
+// Imports: API Key
+
+
+
 // GraphQL: Resolvers
 const RESOLVERS = {
   Query: {
-    sampleFunction: (parent, args) => {
+    test: (parent, args) => {
       return axios.get(``)
       .then((response) => response.data)
       .catch((error) => console.log(error))
     }
   }
-}
+};
 
 // Exports
 export default RESOLVERS;
